7a8f76e8da10d2e30d033ab853d85619ffad065c
Jakub Mazuch <jakub.mazuch@gmail.com>
2024-11-14 15:26:07 +0100

    14.11.2024, 15:27

--- lekce_06/grid_ctverec.py
+++ lekce_06/grid_ctverec.py
@@ -7,13 +7,24 @@ def ctverec(velikost):
         turtle.right(90)
 
 
+def grid(velikost, pocet):
+    for y in range(pocet):
+        for x in range(pocet):
+            ctverec(velikost)
+            turtle.forward(velikost)
+        turtle.backward(velikost * pocet)
+        turtle.right(90)
+        turtle.forward(velikost)
+        turtle.left(90)
+
+
 # nastavení želvy
 turtle.shape("turtle")
-turtle.speed(1)
+turtle.speed(100)
 turtle.penup()
 turtle.goto(-100, 100)
 turtle.pendown()
 
-ctverec(25)
+grid(10, 5)
 
 turtle.done()
